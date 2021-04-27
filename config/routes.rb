5c644c6dd5384a2cadce49a78a1bff564d170bf6
UwUbulachka <meuw25@gmail.com>
2021-04-27 19:59:03 +0300

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
    resource :contacts, only: [:new, :create], path_names: { :new => '' }
 
-   resources :articles
+  resources :articles do 
+    resources :comments
+  end 
 end
